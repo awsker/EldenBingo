d4cdd3affeaa44fbd12d4fde47a8aa30ece03356
unknown <asker@sealhunter.com>
2024-03-16 14:33:56 +0100

    Changed some settings

--- EldenBingo/Properties/Settings.Designer.cs
+++ EldenBingo/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace EldenBingo.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.2.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.7.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -421,7 +421,7 @@ namespace EldenBingo.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("True")]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
         public bool ClickIncrementsCountedSquares {
             get {
                 return ((bool)(this["ClickIncrementsCountedSquares"]));
