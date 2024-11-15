58cd8fd9f2ae231ca0ed9a18f8b618ee54db3b48
asker <asker@sealhunter.com>
2024-11-15 04:31:26 +0100

    No longer locks dispatch thread when opening lobby settings form

--- EldenBingo/UI/AdminControl.cs
+++ EldenBingo/UI/AdminControl.cs
@@ -277,7 +277,8 @@ namespace EldenBingo.UI
 
         private void receivedGameSettings(ClientModel? _, ServerCurrentGameSettings gameSettingsArgs)
         {
-            openSettingsWindow(gameSettingsArgs.GameSettings);
+            //Open the settings window without locking the receiver thread
+            Task.Run(() => openSettingsWindow(gameSettingsArgs.GameSettings));
         }
 
         private void openSettingsWindow(BingoGameSettings settings)
@@ -299,7 +300,7 @@ namespace EldenBingo.UI
                     var form = new GameSettingsForm();
                     form.Settings = settings;
 
-                    if (form.ShowDialog(this) == DialogResult.OK)
+                    if (form.ShowDialog(mainForm) == DialogResult.OK)
                     {
                         GameSettingsHelper.SaveToSettings(form.Settings, Properties.Settings.Default);
                         var request = new ClientSetGameSettings(form.Settings);
